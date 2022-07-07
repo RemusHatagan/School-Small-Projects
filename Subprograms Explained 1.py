f46c9deeb4e6875a8b3c723288e3b4eb88c17ab2
Remus Hatagan <97230349+RemusHatagan@users.noreply.github.com>
2022-07-07 11:53:04 +0100

    Add files via upload

---
+++ Subprograms Explained 1.py
@@ -0,0 +1,20 @@
+
+# Subprograms
+
+# This subprogram is an example of a "procedure"
+# A procedure does NOT have a "return" statement / value
+
+def kebab():  # The brackets are called parameters
+    print("")
+    print("Kebabs are TASTY!!")
+    print("Yay for the Kebab")
+    print("Kebabs are awseome!!")
+    print("Kebabs and chilli sauce!!")
+    print("")
+
+kebab()   #}
+kebab()   #} These are all calls
+kebab()   #}
+
+
+
